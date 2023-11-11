b7982d30c412f67f45308aaeb5beb72a120b622d
Anton3aPro100 <kironsdaddy@gmail.com>
2023-11-11 15:50:30 +0300

    third iteration

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -58,43 +58,14 @@ class SearchServer {
 }; 
 
 void AddDocument(const int& document_id, const string& document) { 
-/* Я много и долго пытался переписать этот метод, пытался и сделать все в одном цикле,
-и считать до цикла 1.size, и использовать мтод из библиотеки алгоритмов для подсчета количества въождений эемента в вектор. И у всех этих попыток тренажер засчитывал только 11 из 18 и писал подсказку что реализация слишком медленная.
-А как было писал - Успех!
-Поэтому оставил как есть. 
-
-Так же не понял чем надо заменить insert для множества.
-
-Не могли бы вы засчитать эту версию, все-таки достаточно логичные действия там, и два цикла не вложенны же друг в друга...
-А то я и так еле еле успеваю к дедлайну...
-И мне очень интересно просто увидеть как эту функцию можно более оптиально написать
-*/ 
-        const vector<string> words = SplitIntoWordsNoStop(document); 
-        double words_size=words.size(); 
-        map <string,int> tf; 
-
-        for (const string& word: words){ 
-
-            if (tf.count(word)>0) { 
-
-                tf[word]+=1; 
-
-            } 
-
-            else{ 
-
-                tf.insert({word,1}); 
-
-            } 
-        } 
-
-        for (pair<string,int> word_tf:tf) { 
-
-            index_[word_tf.first].insert({document_id,double(word_tf.second)/words_size}); 
-    } 
+    const vector<string> words = SplitIntoWordsNoStop(document); 
+    double size=words.size(); 
+    //++documents_count_; Когда пишу такую строчку тренажер снова пишет медленно... Хотя не могу понять как тренажер вобще может обнаружить такое незначительное изменение времени работы
+    for (const string& word: words){
+        index_[word][document_id] += 1/size;
+        // Да, так гораздо короче и лучше смотрится! С одним циклом и с //map<string,map<...>> вместо map<string,map<set<pair<...>>
+    }
 }
-
-
     vector<Document> FindTopDocuments(const string& raw_query) const  {
         const Query query_words = ParseQuery(raw_query); 
         auto matched_documents = FindAllDocuments(query_words); 
@@ -107,23 +78,23 @@ void AddDocument(const int& document_id, const string& document) {
         return matched_documents; 
     } 
 
- void SetDocumentsCount(int documents_count){ 
+ void SetDocumentsCount(int documents_count){
+/* она мне нужна, потому что documents_count_ - private , 
+но используется только один раз при создании обьекта класса,
+ вместо того, чтобы при каждом добавлении документа увеличивать на 1. 
+Это и не логично, ведь количество документов в этой задаче просто задается изначально, зачем их считать?...
+*/
      documents_count_=documents_count; 
  } 
 
 private: 
-    map<string,set<pair<int,double>>> index_; 
+    map<string,map<int,double>> index_;// заменил set на map стало логичнее  и короче 
     set<string> stop_words_; 
     int documents_count_=0; 
     double idf_calculate(const string& word) const{
-    // написал отдельный метод для подсчета IDF, но если его применяю, вместо вычислений 
-//без функции, тренажер пишет, что слишком медленно, хотя теперь только в одной из задач.
-// Думаю, что разница незначительна, но так тренажер видимо настроен 
-       
-        
-        double idf=double(documents_count_); 
-        idf/=index_.at(word).size(); 
-        idf=log(idf); 
+           
+        double idf=log(static_cast<double>(documents_count_)/index_.at(word).size()); 
+         // сделал в одну строку, про static_cast<Type> постараюсь не забывать
         return idf;
     }
     bool IsStopWord(const string& word) const { 
@@ -162,10 +133,8 @@ private:
         for (int id=0; id<documents_count_;++id ) documents_hit[id]=-1; 
         for (const string& word : query_words.plus_words){
             if (index_.count(word)>0){
-               double idf=double(documents_count_); 
-               idf/=index_.at(word).size(); 
-               idf=log(idf); 
-               
+               double idf=idf_calculate(word);
+              //Да, все ок, просто в прошлый раз я ее зачем-то в цикле каждый раз вызывал
            for (const pair<int,double>& tf :index_.at(word) ){ 
                if (documents_hit[tf.first]==-1)  documents_hit[tf.first]=0; 
                 documents_hit[tf.first]+=tf.second*idf; 
